5bbdc5c98049307f24dac3d0192564250af81ccb
Yoonseoyi <20220994@sungshin.ac.kr>
2024-09-12 08:29:38 +0900

    Add files via upload

---
+++ src/services/api/requests.ts
@@ -0,0 +1,141 @@
+import addBaseUrlPrefix from './addBaseUrlPrefix'
+
+const requestInterceptor = (url: string, method: string) => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`🚀 [API] ${method} ${url} | Request`)
+  }
+}
+
+const responseInterceptor = (url: string, method: string, response: Response) => {
+  if (process.env.NODE_ENV === 'development') {
+    if (response.ok) {
+      console.log(`🚁 [API] ${method} ${url} | Response ${response.status}`)
+    } else {
+      console.log(`🚨 [API] ${method} ${url} | Error ${response.status} ${response.statusText}`)
+    }
+  }
+}
+
+export const getRequest = async <T>(url: string, config?: RequestInit): Promise<T> => {
+  requestInterceptor(url, 'GET')
+  const response = await fetch(await addBaseUrlPrefix(url), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...config,
+  })
+  responseInterceptor(url, 'GET', response)
+
+  if (!response.ok) {
+    let errorMessage = '알 수 없는 오류가 발생했습니다'
+    try {
+      const errorData = await response.json()
+      errorMessage = errorData.message
+    } catch (error) {
+    }
+    return Promise.reject(new Error(`${response.status}: ${errorMessage}`))
+  }
+
+  return response.json() as Promise<T>
+}
+
+export const postRequest = async <T, B>(url: string, body?: B, config?: RequestInit): Promise<T> => {
+  requestInterceptor(url, 'POST')
+  const response = await fetch(await addBaseUrlPrefix(url), {
+    method: 'POST',
+    body: body instanceof FormData ? body : JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...config,
+  })
+  responseInterceptor(url, 'POST', response)
+
+  if (!response.ok) {
+    let errorMessage = '알 수 없는 오류가 발생했습니다'
+    try {
+      const errorData = await response.json()
+      errorMessage = errorData.message
+    } catch (error) {
+    }
+    return Promise.reject(new Error(`${response.status}: ${errorMessage}`))
+  }
+
+  return response.json() as Promise<T>
+}
+
+export const putRequest = async <T, B>(url: string, body: B, config?: RequestInit): Promise<T> => {
+  requestInterceptor(url, 'PUT')
+  const response = await fetch(await addBaseUrlPrefix(url), {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...config,
+  })
+  responseInterceptor(url, 'PUT', response)
+
+  if (!response.ok) {
+    let errorMessage = '알 수 없는 오류가 발생했습니다'
+    try {
+      const errorData = await response.json()
+      errorMessage = errorData.message
+    } catch (error) {
+    }
+    return Promise.reject(new Error(`${response.status}: ${errorMessage}`))
+  }
+
+  return response.json() as Promise<T>
+}
+
+export const deleteRequest = async <T, B>(url: string, body?: B, config?: RequestInit): Promise<T> => {
+  requestInterceptor(url, 'DELETE')
+  const response = await fetch(await addBaseUrlPrefix(url), {
+    method: 'DELETE',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...config,
+  })
+  responseInterceptor(url, 'DELETE', response)
+
+  if (!response.ok) {
+    let errorMessage = '알 수 없는 오류가 발생했습니다'
+    try {
+      const errorData = await response.json()
+      errorMessage = errorData.message
+    } catch (error) {
+    }
+    return Promise.reject(new Error(`${response.status}: ${errorMessage}`))
+  }
+
+  return response.json() as Promise<T>
+}
+
+export const patchRequest = async <T, B>(url: string, body: B, config?: RequestInit): Promise<T> => {
+  requestInterceptor(url, 'PATCH')
+  const response = await fetch(await addBaseUrlPrefix(url), {
+    method: 'PATCH',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...config,
+  })
+  responseInterceptor(url, 'PATCH', response)
+
+  if (!response.ok) {
+    let errorMessage = '알 수 없는 오류가 발생했습니다'
+    try {
+      const errorData = await response.json()
+      errorMessage = errorData.message
+    } catch (error) {
+    }
+    return Promise.reject(new Error(`${response.status}: ${errorMessage}`))
+  }
+
+  return response.json() as Promise<T>
+}
